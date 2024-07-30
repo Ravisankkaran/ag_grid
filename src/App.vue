d32607a81e4710b7d82026a2723b7a8a4a9b7a3f
Ravisankkaran <ravisankkaran@gmail.com>
2024-07-30 15:45:09 +0530

    added styles

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         style="height: 500px;"
         :columnDefs="columnDefs"
         :rowData="rowData"
-         :gridOptions="gridOptions"
+        :gridOptions="gridOptions"
         @grid-ready="onGridReady"
       >
       </ag-grid-vue>
@@ -43,24 +43,33 @@ export default {
       rowData: [], // row data is just declared because data will be imported from Fake Store API
       columnDefs: [
         { field: 'id', headerName: 'ID', hide: true },
-        { field: 'title', headerName: 'Product Title', width: 350, minWidth: 200,cellClass: 'center-text' },
-        { field: 'price', headerName: 'Price', width: 150, minWidth: 100 ,cellClass: 'center-text'},
-        { field: 'category', headerName: 'Category', width: 200, minWidth: 100,cellClass: 'center-text' },
-        { field: 'rating.count', headerName: 'Avilabe Quantity', width: 150, minWidth: 100 ,cellClass: 'center-text'},
+        { field: 'title', headerName: 'Product Title', width: 350, minWidth: 200,cellClass: 'center-text',wrapText: true },
         { field: 'image', headerName: 'Image', width:150,
-         cellRenderer: params => `<img src="${params.value}" style="width: 100px; height: 100px;" />` 
+         cellRenderer: params => `<img src="${params.value}" style="width: 100px; height: 100px ;     padding: 10px;" />` 
+        },
+        { field: 'price', headerName: 'Price', width: 150, minWidth: 100 ,cellClass: 'center-text',  cellRenderer: params => `$ ${params.value}`},
+        { field: 'category', headerName: 'Category', width: 200, minWidth: 100,cellClass: 'center-text' },
+        { field: 'rating.rate', headerName: 'Rating', width: 150, minWidth: 100 , cellClass: 'center-text', cellRenderer: this.starRatingRenderer},
+        { 
+          field: 'rating.count',
+          headerName: 'Status',
+          width: 150,
+          minWidth: 100,
+          cellClass: 'center-text',
+          cellRenderer: this.statusRenderer
         },
         {
           // column that render cell with default command (user declare)
           field: 'action',
           headerName: 'Action',
           cellRenderer: 'EditButton',
+          cellClass: 'center-text',
           width: 100,
           minWidth: 80
         }
       ],
       gridOptions: {
-        rowHeight: 100  // Set row height to 40px
+        rowHeight: 100  // Set row height to 100px
       },
       // initialize variable as null/false at initial point
       gridApi: null,
@@ -81,6 +90,45 @@ export default {
       });
   },
   methods: {
+    starRatingRenderer(params) {
+      const rating = params.value;
+      const maxStars = 5; // Maximum number of stars
+      let stars = '';
+
+      for (let i = 0; i < maxStars; i++) {
+        stars += `<i class="fa ${i < rating ? 'fa-star' : 'fa-star-o'}"></i>`;
+      }
+
+      return stars;
+    },
+    statusRenderer(params) {
+      const count = params.value;
+    const eDiv = document.createElement('div');
+    
+    if (count > 150) {
+      eDiv.innerHTML = 'Available';
+      eDiv.style.backgroundColor = 'lightgreen';
+      eDiv.style.color = 'green';
+      // eDiv.style.border = '2px solid lightgreen';
+      eDiv.style.padding = '5px';
+      eDiv.style.borderRadius ='10px';
+      eDiv.style.textAlign = 'center';
+      eDiv.style.fontWeight= 'bold';
+      eDiv.style.padding='0 10px';
+    } else {
+      eDiv.innerHTML = 'Out of Stock';
+      eDiv.style.backgroundColor = 'lightcoral';
+      eDiv.style.color = 'red';
+      eDiv.style.borderRadius ='10px';
+      eDiv.style.fontWeight= 'bold';
+      // eDiv.style.border = '2px solid lightcoral';
+      eDiv.style.padding = '5px';
+      eDiv.style.textAlign = 'center';
+      eDiv.style.padding=' 0 10px';
+    }
+
+    return eDiv;
+    },
     // writing methods for each function
     addProductToGrid(product) {
       this.rowData = [...this.rowData, product];
@@ -110,3 +158,4 @@ export default {
   }
 }
 </script>
+
--- src/components/ImageRender.vue
+++ src/components/ImageRender.vue
@@ -13,3 +13,8 @@ export default{
   }
 }
 </script>
+<style scoped>
+img{
+    padding: 20px;
+}
+</style>
